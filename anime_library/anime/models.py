82048475c565fc24af474192fb71c8a72a17a7ff
Milk <145270243+Milky47Way@users.noreply.github.com>
2026-02-23 19:33:42 +0200

    Add files via upload

--- anime_library/anime/models.py
+++ anime_library/anime/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Genres(models.Model):
-    genre = models.CharField(max_length=50)
+    genre = models.CharField("Жанр", max_length=50)
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанри"
+        ordering = ['genre']
 
     def __str__(self):
         return self.genre
@@ -12,33 +17,42 @@ class Genres(models.Model):
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
-
-
-class Anime(models.Model):
-    title = models.CharField(max_length=250)
-    poster = models.ImageField(upload_to='images/posters', verbose_name='Лого')
-    description = models.CharField(max_length=50)
-    genre = models.ForeignKey(Genres, on_delete=models.SET_NULL, null=True, blank=True, related_name="anime")
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=True, related_name="anime")
-    release = models.DateTimeField
-
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Автори"
 
     def __str__(self):
-        return self.title
+        return self.author
 
 class DubStudio(models.Model):
     name = models.CharField(max_length=100)
     language = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Студія дубляжу"
+        verbose_name_plural = "Студії дубляжу"
+
     def __str__(self):
         return self.name
 
-# class Image(models.Model):
+class Anime(models.Model):
+    title = models.CharField(max_length=250)
+    poster = models.ImageField(upload_to='images/posters', verbose_name='Лого')
+    description = models.TextField()
 
+    genre = models.ForeignKey(Genres, on_delete=models.SET_NULL, null=True, blank=True, related_name="anime")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=True, related_name="authored_anime")
+    dubStudio = models.ForeignKey("DubStudio", on_delete=models.SET_NULL, null=True)
 
-class Trailer(models.Model):
+    release = models.DateTimeField(verbose_name="Рік релізу")
+    trailer_url = models.URLField(blank=True)
 
+    rating = models.DecimalField(max_digits=3, decimal_places=1, default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
 
-class Rating(models.Model):
+    class Meta:
+        verbose_name = "Аніме"
+        verbose_name_plural = "Аніме"
+
+    def __str__(self):
+        return self.name
+
